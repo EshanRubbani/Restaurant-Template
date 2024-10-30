c36cc1a0c5dc17305f204a8debf4ae7d364813b8
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-10-30 15:45:28 +0500

    Create fourthSection.dart

---
+++ lib/views/Homepage/widgets/FourthSection/fourthSection.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:restro_web/views/Homepage/widgets/FirstSection/first_section_desktop.dart';
+import 'package:restro_web/views/Homepage/widgets/FirstSection/first_section_mobile.dart';
+import 'package:restro_web/views/Homepage/widgets/FourthSection/fourthsectionDesktop.dart';
+import 'package:restro_web/views/Homepage/widgets/FourthSection/fourthsectionMobile.dart';
+import 'package:restro_web/views/Homepage/widgets/SecondSection/second_section_desktop.dart';
+import 'package:restro_web/views/Homepage/widgets/SecondSection/second_section_mobile.dart';
+import 'package:restro_web/views/Homepage/widgets/ThirdSection/thirdsectionDesktop.dart';
+import 'package:restro_web/views/Homepage/widgets/ThirdSection/thirdsectionMobile.dart';
+// Import your CenteredView file
+
+class FourthSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 850) {
+          return FourthsectionDesktop();
+        } else {
+          return FourthsectionMobile();
+        }
+      },
+    );
+  }
+}
