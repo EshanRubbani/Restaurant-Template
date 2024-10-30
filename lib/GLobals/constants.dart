012d31311457ce4929a3b96c0ddb012ad27bf250
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-10-30 15:44:06 +0500

    Create constants.dart

---
+++ lib/GLobals/constants.dart
@@ -0,0 +1,19 @@
+// constant.dart
+
+class ApiConstants {
+  // Base URL for the API
+  static const String baseUrl =
+      'https://cloudapi.ovvihq.com/api/OnlineAPIOrder/GetOnlineMenu';
+  static const String getMenu =
+      'https://cloudapi.ovvihq.com/api/OnlineAPIOrder/GetMenu';
+        static const String GetImageByItemNumber=
+      'https://cloudapi.ovvihq.com/api/OnlineAPIOrder/GetImageByItemNumber/';
+              static const String GetItemDetailByItemNumber=
+      'https://cloudapi.ovvihq.com/api/OnlineAPIOrder/GetItemDetailbyItemNumbers';
+                                static const String CustomerSignUp=
+      'https://cloudapi.ovvihq.com/api/Account/CustomerSignUp';
+
+  // API Token
+  static const String token =
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBUElLZXkiOiI3MWVjYWE2Ny1lOGViLTRkOWMtYjZiYi1iNDkyMTA5ODgzYTciLCJleHAiOjE4MDAxODQzNDJ9.lzn9sn4nWIg6GMhfMlQ5HZLQCKsdCmIxJdX35HSh870';
+}
