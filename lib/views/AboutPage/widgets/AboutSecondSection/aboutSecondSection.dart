5c84593252196162a83bc65c1c94c3164666a283
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-10-30 15:44:26 +0500

    Create aboutSecondSection.dart

---
+++ lib/views/AboutPage/widgets/AboutSecondSection/aboutSecondSection.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:restro_web/views/AboutPage/widgets/AboutFirstSection/aboutfirstsectionDesktop.dart';
+import 'package:restro_web/views/AboutPage/widgets/AboutFirstSection/aboutfirstsectionMobile.dart';
+import 'package:restro_web/views/AboutPage/widgets/AboutSecondSection/aboutSecondDesktop.dart';
+import 'package:restro_web/views/AboutPage/widgets/AboutSecondSection/aboutSecondMobile.dart';
+import 'package:restro_web/views/Homepage/widgets/CarasoulSlider/carasoulsliderDesktop.dart';
+import 'package:restro_web/views/Homepage/widgets/CarasoulSlider/carasoulsliderMobile.dart';
+import 'package:restro_web/views/Homepage/widgets/FirstSection/first_section_desktop.dart';
+import 'package:restro_web/views/Homepage/widgets/FirstSection/first_section_mobile.dart';
+import 'package:restro_web/views/Homepage/widgets/SecondSection/second_section_desktop.dart';
+import 'package:restro_web/views/Homepage/widgets/SecondSection/second_section_mobile.dart';
+// Import your CenteredView file
+
+class Aboutsecondsection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 850) {
+          return AboutSecondDesktop();
+        } else {
+          return AboutSecondMobile();
+        }
+      },
+    );
+  }
+}
