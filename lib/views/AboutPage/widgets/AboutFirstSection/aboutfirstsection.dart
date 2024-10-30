41b4d9f05953a2e35aba7d6cca938631d4289aa1
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-10-30 15:44:19 +0500

    Create aboutfirstsection.dart

---
+++ lib/views/AboutPage/widgets/AboutFirstSection/aboutfirstsection.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:restro_web/views/AboutPage/widgets/AboutFirstSection/aboutfirstsectionDesktop.dart';
+import 'package:restro_web/views/AboutPage/widgets/AboutFirstSection/aboutfirstsectionMobile.dart';
+import 'package:restro_web/views/Homepage/widgets/CarasoulSlider/carasoulsliderDesktop.dart';
+import 'package:restro_web/views/Homepage/widgets/CarasoulSlider/carasoulsliderMobile.dart';
+import 'package:restro_web/views/Homepage/widgets/FirstSection/first_section_desktop.dart';
+import 'package:restro_web/views/Homepage/widgets/FirstSection/first_section_mobile.dart';
+import 'package:restro_web/views/Homepage/widgets/SecondSection/second_section_desktop.dart';
+import 'package:restro_web/views/Homepage/widgets/SecondSection/second_section_mobile.dart';
+// Import your CenteredView file
+
+class Aboutfirstsection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 850) {
+          return AboutFirstsectionDesktop();
+        } else {
+          return AboutfirstsectionMobile();
+        }
+      },
+    );
+  }
+}
