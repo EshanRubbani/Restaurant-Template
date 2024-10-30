4ef978e93ce0d7ba5a5be638ce3f8be8830447bc
Eshan Rubbani <138296790+EshanRubbani@users.noreply.github.com>
2024-10-30 15:45:33 +0500

    Create second_section.dart

---
+++ lib/views/Homepage/widgets/SecondSection/second_section.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:restro_web/views/Homepage/widgets/FirstSection/first_section_desktop.dart';
+import 'package:restro_web/views/Homepage/widgets/FirstSection/first_section_mobile.dart';
+import 'package:restro_web/views/Homepage/widgets/SecondSection/second_section_desktop.dart';
+import 'package:restro_web/views/Homepage/widgets/SecondSection/second_section_mobile.dart';
+// Import your CenteredView file
+
+class SecondSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 850) {
+          return SecondSectionDesktop();
+        } else {
+          return SecondSectionMobile();
+        }
+      },
+    );
+  }
+}
